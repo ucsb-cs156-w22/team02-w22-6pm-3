0f92025e0fba65d90d904f2b9a58ac6c08896228
Johnson Lin <yongchen@ucsb.edu>
2022-02-05 14:45:12 -0800

    tests not working yet

--- src/test/java/edu/ucsb/cs156/team02/controllers/UCSBSubjectControllerTests.java
+++ src/test/java/edu/ucsb/cs156/team02/controllers/UCSBSubjectControllerTests.java
@@ -30,9 +30,9 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
-@WebMvcTest(controllers = TodosController.class)
+@WebMvcTest(controllers = UCSBSubjectController.class)
 @Import(TestConfig.class)
-public class TodosControllerTests extends ControllerTestCase {
+public class UCSBSubjectControllerTests extends ControllerTestCase {
 
      @MockBean
     UCSBSubjectRepository uCSBSubjectRepository;
@@ -42,8 +42,31 @@ public class TodosControllerTests extends ControllerTestCase {
 
     @Test
     public void api_UCSBSubject_admin_all__logged_out__returns_403() throws Exception {
-        mockMvc.perform(get("/api/UCSBSubjects/all"))
+        mockMvc.perform(get("/api/UCSBSubject/admin/all"))
                 .andExpect(status().is(403));
     }
 
+
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void api_UCSBSubject__user_logged_in__returns_a_UCSBSubject_that_exists() throws Exception {
+
+        // arrange
+
+        User u = currentUserService.getCurrentUser().getUser();
+        UCSBSubject ucsbSubject1 = UCSBSubject.builder().subjectCode("UCSBSubject 1").subjectTranslation("UCSBSubject 1").deptCode("UCSBSubject 1").collegeCode("UCSBSubject 1").relatedDeptCode("UCSBSubject 1").inactive(false).id(7L).build();
+        when(uCSBSubjectRepository.findById(eq(7L))).thenReturn(Optional.of(ucsbSubject1));
+
+        // act
+        MvcResult response = mockMvc.perform(get("/api/ucsb_subjects?id=7"))
+                .andExpect(status().isOk()).andReturn();
+
+        // assert
+
+        verify(uCSBSubjectRepository, times(1)).findById(eq(7L));
+        String expectedJson = mapper.writeValueAsString(ucsbSubject1);
+        String responseString = response.getResponse().getContentAsString();
+        assertEquals(expectedJson, responseString);
+    }
 }
