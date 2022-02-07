d10ef49a50d9009795cac03af257daabc653c2c4
Erin Ambriz <erinambriz@ucsb.edu>
2022-02-06 16:45:39 -0800

    Implemented Requirements Controller and tests, working on local host

--- src/main/java/edu/ucsb/cs156/team02/repositories/UCSBRequirementRepository.java
+++ src/main/java/edu/ucsb/cs156/team02/repositories/UCSBRequirementRepository.java
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UCSBRequirementRepository extends CrudRepository<UCSBRequirement, Long> {
-  Optional<UCSBRequirement> findById(Long id);
+  //Iterable<UCSBRequirement> findAllByUserId(Long user_id);
 }
