8119d3646f9312fa7b6df31dbc56f0665fd8bd8f
Rose Lin <tianyi_lin@ucsb.edu>
2022-02-07 13:07:39 -0800

    rl-sm-fixing mutant tests

--- src/test/java/edu/ucsb/cs156/team02/controllers/CollegiateSubredditControllerTests.java
+++ src/test/java/edu/ucsb/cs156/team02/controllers/CollegiateSubredditControllerTests.java
@@ -33,72 +33,91 @@ import static org.mockito.Mockito.when;
 @Import(TestConfig.class)
 public class CollegiateSubredditControllerTests extends ControllerTestCase {
 
-    @MockBean
-    CollegiateSubredditRepository collegiateSubredditRepository;
+        @MockBean
+        CollegiateSubredditRepository collegiateSubredditRepository;
 
-    @MockBean
-    UserRepository userRepository;
+        @MockBean
+        UserRepository userRepository;
 
-    // No authorization tests needed for /api/collegiateSubreddits/admin/all
-    // ^ or at least...i think ??????????????
+        // No authorization tests needed for /api/collegiateSubreddits/admin/all
+        // ^ or at least...i think ??????????????
 
-    // Authorization tests for /api/collegiateSubreddits/all
+        // Authorization tests for /api/collegiateSubreddits/all
 
-    @Test
-    public void api_collegiateSubreddits_all__logged_out__returns_403() throws Exception {
-        mockMvc.perform(get("/api/collegiateSubreddits/all"))
-                .andExpect(status().is(403));
-    }
+        @Test
+        public void api_collegiateSubreddits_all__logged_out__returns_403() throws Exception {
+                mockMvc.perform(get("/api/collegiateSubreddits/all"))
+                                .andExpect(status().is(403));
+        }
 
-    @WithMockUser(roles = { "USER" })
-    @Test
-    public void api_collegiateSubreddits_all__user_logged_in__returns_200() throws Exception {
-        mockMvc.perform(get("/api/collegiateSubreddits/all"))
-                .andExpect(status().isOk());
-    }
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void api_collegiateSubreddits_all__user_logged_in__returns_200() throws Exception {
+                mockMvc.perform(get("/api/collegiateSubreddits/all"))
+                                .andExpect(status().isOk());
+        }
 
-    // Authorization tests for /api/collegiateSubreddits/post
-    // Only users (and admin, I'm assuming) can post
-    @Test
-    public void api_collegiateSubreddits_post__logged_out__returns_403() throws Exception {
-        mockMvc.perform(post("/api/todos/post"))
-                .andExpect(status().is(403));
-    }
+        // Authorization tests for /api/collegiateSubreddits/post
+        // Only users (and admin, I'm assuming) can post
+        @Test
+        public void api_collegiateSubreddits_post__logged_out__returns_403() throws Exception {
+                mockMvc.perform(post("/api/todos/post"))
+                                .andExpect(status().is(403));
+        }
 
+        // Tests with mocks for database actions
 
-    // Tests with mocks for database actions
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void api_collegiateSubreddits_post__user_logged_in() throws Exception {
+                // arrange
+                CollegiateSubreddit expectedCollegiateSubreddit = CollegiateSubreddit.builder()
+                                .name("Test Name")
+                                .location("Test Location")
+                                .subreddit("Test Subreddit")
+                                .id(1L)
+                                .build();
 
-    @WithMockUser(roles = { "USER" })
-    @Test
-    public void api_collegiateSubreddits_post__user_logged_in() throws Exception {
-        // arrange
+                when(collegiateSubredditRepository.save(eq(expectedCollegiateSubreddit)))
+                                .thenReturn(expectedCollegiateSubreddit);
 
-        //User u = currentUserService.getCurrentUser().getUser();
-        long u_id = currentUserService.getCurrentUser().getUser().getId();
+                // act
+                MvcResult response = mockMvc.perform(
+                                post("/api/collegiateSubreddits/post?name=Test Name&location=Test Location&subreddit=Test Subreddit")
+                                                .with(csrf()))
+                                .andExpect(status().isOk()).andReturn();
 
+                // assert
+                verify(collegiateSubredditRepository, times(1)).save(expectedCollegiateSubreddit);
+                String expectedJson = mapper.writeValueAsString(expectedCollegiateSubreddit);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(expectedJson, responseString);
+        }
 
-        CollegiateSubreddit expectedCollegiateSubreddit = CollegiateSubreddit.builder()
-                .name("Test Name")
-                .location("Test Location")
-                .subreddit("Test Subreddit")
-                //.user(u)
-                .id(u_id)
-                //.id(1L)
-                //.id(0L)
-                .build();
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void api_todos_all__user_logged_in__returns_only_todos_for_user() throws Exception {
 
-        when(collegiateSubredditRepository.save(eq(expectedCollegiateSubreddit))).thenReturn(expectedCollegiateSubreddit);
+                // arrange
 
-        // act
-        MvcResult response = mockMvc.perform(
-                post("/api/collegiateSubreddits/post?name=Test Name&location=Test Location&subreddit=Test Subreddit")
-                        .with(csrf()))
-                .andExpect(status().isOk()).andReturn();
+                User thisUser = currentUserService.getCurrentUser().getUser();
 
-        // assert
-        verify(collegiateSubredditRepository, times(1)).save(expectedCollegiateSubreddit);
-        String expectedJson = mapper.writeValueAsString(expectedCollegiateSubreddit);
-        String responseString = response.getResponse().getContentAsString();
-        assertEquals(expectedJson, responseString);
-    }
+                Todo todo1 = Todo.builder().title("Todo 1").details("Todo 1").done(false).user(thisUser).id(1L).build();
+                Todo todo2 = Todo.builder().title("Todo 2").details("Todo 2").done(false).user(thisUser).id(2L).build();
+
+                ArrayList<Todo> expectedTodos = new ArrayList<>();
+                expectedTodos.addAll(Arrays.asList(todo1, todo2));
+                when(todoRepository.findAllByUserId(thisUser.getId())).thenReturn(expectedTodos);
+
+                // act
+                MvcResult response = mockMvc.perform(get("/api/todos/all"))
+                                .andExpect(status().isOk()).andReturn();
+
+                // assert
+
+                verify(todoRepository, times(1)).findAllByUserId(eq(thisUser.getId()));
+                String expectedJson = mapper.writeValueAsString(expectedTodos);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(expectedJson, responseString);
+        }
 }
